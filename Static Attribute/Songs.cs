009a216833d296d735ad3246efd17dbb60c882ba
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-18 22:31:07 +0200

    Add project files.

---
+++ Static Attribute/Songs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Attribute
+{
+    internal class Songs
+    {
+        public string title ;
+        public string artist ;
+        public int duration;
+
+        public static int songcount;// declared a static attribute
+        public Songs(string aTitle, string aArtist, int aDuration) // Constructor
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songcount++;
+
+        }
+
+    }
+}
